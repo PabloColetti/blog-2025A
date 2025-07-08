833f4ac3297a63a4dc9d25cf501e469a02c2c86f
pabloinformatorio <pabloinformatorio@gmail.com>
2025-07-08 10:11:21 -0300

    Add configuration files for Django project: base, local, and production settings

---
+++ blog/blog/configurations/production.py
@@ -0,0 +1,32 @@
+from .base import *
+
+DEBUG = False
+
+# TODO: Configurar el dominio al hacer deploy a production
+ALLOWED_HOSTS = ["midominio-production.com"]
+
+# TODO: Configurar db para production
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
+        # En caso de usar una postgres
+        # 'ENGINE': 'django.db.backends.postgresql',
+
+        # En caso de usar una mysql
+        # 'ENGINE': 'django.db.backends.mysql',
+
+        # 'NAME': os.getenv('DB_NAME'),
+        
+        # 'USER': os.getenv('DB_USER'),
+
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
+
+        # 'HOST': os.getenv('DB_HOST'),
+
+        # 'PORT': os.getenv('DB_PORT'),
+    }
+}
+
+os.environ['DJANGO_PORT'] = '8080'
