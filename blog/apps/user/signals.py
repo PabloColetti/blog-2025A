f0e71eb7fa3fe8a8fac30771ed8d565584ef457c
pabloinformatorio <pabloinformatorio@gmail.com>
2025-07-22 10:06:22 -0300

    Update migration timestamp and add user group and permission handling

---
+++ blog/apps/user/signals.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.models import Permission, Group
+from django.contrib.contenttypes.models import ContentType
+from apps.user.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+@receiver(post_save, sender=User)
+def create_groups_and_permissions(sender, instance, created, **kwargs):
+    if created and instance.is_superuser:
+        try:
+            # TODO: Definir los permisos de POSTS y de COMMENTS
+
+            # Crear grupos de usuarios registrados
+            registered_group, created = Group.objects.get_or_create(
+                name="Registered"
+            )
+            registered_group.permissions.add(
+                # permiso para ver post 
+                # permiso para ver comentarios del post 
+                # permiso para crear comentarios del post
+                # permiso para actualizar de su comentario en un post
+                # permiso para borrar su comentario en un post
+            )
+
+            # Crear grupos de usuarios colaboradores
+            registered_group, created = Group.objects.get_or_create(
+                name="Collaborators"
+            )
+            registered_group.permissions.add(
+                # permiso para ver post 
+                # permiso para crear post 
+                # permiso para actualizar su post 
+                # permiso para borrar su post 
+                # permiso para ver comentarios del post 
+                # permiso para crear comentarios del post
+                # permiso para actualizar de su comentario en un post
+                # permiso para borrar su comentario en un post
+            )
+
+            # Crear grupos de usuarios administradores
+            registered_group, created = Group.objects.get_or_create(
+                name="Admins"
+            )
+            registered_group.permissions.add(
+                # permiso para ver post 
+                # permiso para crear post 
+                # permiso para actualizar su post 
+                # permiso para borrar cualquier post 
+                # permiso para ver comentarios del post 
+                # permiso para crear comentarios del post
+                # permiso para actualizar de su comentario en un post
+                # permiso para borrar su comentario de cualquier post
+            )
+
+            print("Grupos y Permisos creados exitosamente.")
+        except ContentType.DoesNotExist:
+            print("El tipo aun no se encuentra disponible.")
+        except Permission.DoesNotExist:
+            print("Uno o mas permisos no se encuentran disponibles")
+            
